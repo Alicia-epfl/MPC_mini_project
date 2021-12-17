2a5168453bd0154a74c906d52e0d42df4326e001
Alicia-epfl <79908084+Alicia-epfl@users.noreply.github.com>
2021-12-17 16:05:41 +0100

    Joli

--- rocket_project/Deliverable_6_1/Deliverable_6_1.m
+++ rocket_project/Deliverable_6_1/Deliverable_6_1.m
@@ -1,3 +1,11 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% EPFL | Semester: fall 2021                 %
+% Alicia Mauroux | sciper: 274618            %
+% ME-425: Model predictive control | part 6  %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 addpath(fullfile('..', 'src'));
 
+
 %% TODO: This file should produce all the plots for the deliverable
+
