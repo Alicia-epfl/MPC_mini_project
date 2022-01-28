fc2b8ed59c245a32c341635f56f8d0d3cb06723a
xav-nal <xavier.nal@epfl.ch>
2022-01-28 10:36:04 +0100

    6.1 28/01/

--- rocket_project/Deliverable_6_1/Deliverable_6_1.m
+++ rocket_project/Deliverable_6_1/Deliverable_6_1.m
@@ -7,21 +7,21 @@ addpath(fullfile('..', 'src'));
 
 Ts = 1/10; % Note that we choose a larger Ts here to speed up the simulation
 rocket = Rocket(Ts);
-H = 1;
+H = 0.5;
 nmpc = NMPC_Control(rocket, H);
 % MPC reference with default maximum roll = 15 deg
 Tf = 30;
-ref = @(t_, x_) rocket.MPC_ref(t_, Tf);
+%ref = @(t_, x_) rocket.MPC_ref(t_, Tf);
 % MPC reference with specified maximum roll = 50 deg
-%roll_max = deg2rad(50);
-%ref = @(t_, x_) rocket.MPC_ref(t_, Tf,roll_max);
+roll_max = deg2rad(50);
+ref = @(t_, x_) rocket.MPC_ref(t_, Tf,roll_max);
 x0 = zeros(12,1);
-display('simulation')
+%display('simulation')
 [T, X, U, Ref] = rocket.simulate_f(x0, Tf, nmpc, ref);
 
 
 
-%% TODO: This file should produce all the plots for the deliverable
+% TODO: This file should produce all the plots for the deliverable
 % Plot pose
 rocket.anim_rate = 1; % Increase this to make the animation faster
 ph = rocket.plotvis(T, X, U, Ref);
--- rocket_project/Deliverable_6_1/NMPC_Control.m
+++ rocket_project/Deliverable_6_1/NMPC_Control.m
@@ -6,6 +6,7 @@ opti = casadi.Opti(); % Optimization problem
 N = ceil(H/rocket.Ts); % MPC horizon
 nx = 12; % Number of states
 nu = 4;  % Number of inputs
+display(N)
 
 % Decision variables (symbolic)
 X_sym = opti.variable(nx, N); % state trajectory
@@ -20,7 +21,7 @@ ref_sym = opti.parameter(4, 1);   % target position
 % Runge-Kutta 4 integration
 f_discrete = @(x,u) RK4(x,u,H,rocket);
 Xs = [0;0;0;0;0;ref_sym(4);0;0;0;ref_sym(1);ref_sym(2);ref_sym(3)];
-display(Xs);
+%display(Xs);
 
 alpha = X_sym(4,:);
 beta = X_sym(5,:);
@@ -31,13 +32,26 @@ Pavg = U_sym(3,:);
 Pdiff = U_sym(4,:);
 % SET THE PROBLEM CONSTRAINTS con AND THE OBJECTIVE obj HERE
             
-R = eye(nu);
+R = 3*eye(nu);
 Q = eye(nx);
+Q(1,1) = 50; %wx
+Q(2,2) = 50; %wy
+Q(3,3) = 1;
+Q(4,4) = 1;  %alpha
+Q(5,5) = 1;  %beta
+Q(6,6) = 1000; %gamma
+Q(7,7) = 1;     % vx;
+Q(8,8) = 1;     % vy;
+Q(9,9) = 1;     % vz;
+Q(10,10) = 1500;
+Q(11,11) = 1500;
+Q(12,12) = 2500;
+%display(Q)
 
 % ---- objective ---------
 obj = 0;
 opti.subject_to(X_sym(:,1)==x0_sym);
-[xs, Us] = rocket.trim()
+[xs, Us] = rocket.trim();
 
 display('RK4')
 for k=1:N-1 % loop over control intervals
@@ -45,16 +59,16 @@ for k=1:N-1 % loop over control intervals
     obj = obj + (X_sym(:,k)-Xs)'*Q*(X_sym(:,k)-Xs) + (U_sym(:,k)-Us)'*R*(U_sym(:,k)-Us); 
 end
 
-opti.subject_to(-0.087 <= alpha <= 0.087); % alpha 
+%opti.subject_to(-0.087 <= alpha <= 0.087); % alpha 
 opti.subject_to(-1.48 <=beta <= 1.48); % beta
 
-display(U_sym);
-display(X_sym);
+%display(U_sym);
+%display(X_sym);
 
 opti.subject_to( -0.26 <= delta1 <= 0.26);
 opti.subject_to(-0.26 <= delta2 <= 0.26);
 
-opti.subject_to(-20 <= Pavg <= 80);
+opti.subject_to(50 <= Pavg <= 80);
 opti.subject_to(-20 <= Pdiff <= 20);
 
 opti.minimize(obj);
